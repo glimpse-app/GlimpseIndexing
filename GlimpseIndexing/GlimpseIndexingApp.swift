0196418ac3289d89607e9fc5ad39d47f72f9f129
ComplexityIsIntelligence <alihafeez431@gmail.com>
2024-06-12 10:59:01 -0400

    Initial Commit

---
+++ GlimpseIndexing/GlimpseIndexingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlimpseIndexingApp.swift
+//  GlimpseIndexing
+//
+//  Created by guestuser on 2024-06-12.
+//
+
+import SwiftUI
+
+@main
+struct GlimpseIndexingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
